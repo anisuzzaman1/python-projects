da3901317f09cb6aed15979dd2a76d0d0adf3e64
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-27 01:49:53 +0600

    Final

--- scrapper.py
+++ scrapper.py
@@ -9,19 +9,19 @@ from tools import wscrape
 
 # ----- Global Variable ----- #
 errorPath = "error_log/error.log"
-urlGrab = "https://www.aljazeera.com/"
+urlGrab = "https://www.prothomalo.com/"
 # ------------------------------- #
 
 # Handle Exception
 wlog.set_custom_log_info(errorPath)
 # --------------------
 
-scapper = wscrape.urlScapper(wscrape.urlGrab, wlog)
-scapper.import_url()
-scapper.export_url()
-scapper.covert_to_bs4()
-scapper.print_data()
-scapper.export_soup_file()
+# scapper = wscrape.urlScapper(wscrape.urlGrab, wlog)
+# scapper.import_url()
+# scapper.export_url()
+# scapper.covert_to_bs4()
+# scapper.print_data()
+# scapper.export_soup_file()
 
 # Try Exception Block Dummy Testing --------
 # try:
--- tools/wscrape.py
+++ tools/wscrape.py
@@ -1,30 +1,35 @@
 from asyncore import file_dispatcher
 from cgitb import html
 from logging import exception
-from msilib.schema import File
+# from msilib.schema import File
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 # Global Variable ------------------ #
-# urlGrab = "https://www.aljazeera.com/"
+urlGrab = "https://www.aljazeera.com/ "
 exportFile = "export/scapped.html"
+exportFile2 = "export/scapped2.html"
 conversionType = "html.parser"
 listObj = ['h1', 'h2', 'h3', 'h4']
+
+
 # -------------------------- #
 
 
-class urlScapper:
+class UrlScappers:
     # Default Value Declation #
     __url__ = ''
     __data__ = ''
     __wlog__ = None
     __soup__ = None
+
     # ---------------------- #
 
     # Constructor Method ------------- #
     def __init__(self, url, wlog):
         self.__url__ = url
         self.__wlog__ = wlog
+
     # ------------------------------- #
 
     # ------ Import Url Block ---------#
@@ -38,6 +43,7 @@ class urlScapper:
             self.__data__ = urlOpen.read()
             if len(self.__data__) > 0:
                 print("Retribe Successfully")
+
     # ---------------------------------- #
 
     # ---- Export/Write File Block  -------------- #
@@ -50,6 +56,7 @@ class urlScapper:
                     fileObj.write(self.__data__)
         except exception as e:
             self.__wlog__.report(e)
+
     # ------------------------------------ #
 
     # ---- Read Local File/Exported File  -------------- #
@@ -59,11 +66,13 @@ class urlScapper:
                 self.__data__ = fileObj.read()
         except exception as e:
             self.__wlog__.report(e)
+
     # ------------------------------------ #
 
     # If Needed to change Url -------- #
     def change_url(self, url):
         self.__url__ = url
+
     # ---------------------------- #
 
     def print_data(self):
@@ -71,8 +80,30 @@ class urlScapper:
 
     # BeautifulSoup Object Process -----#
     def covert_to_bs4(self):
-        self.__soup__ = BeautifulSoup(self.__data__, conversionType)
+        self.__soup__ = BeautifulSoup(self.__data__, "html.parser")
 
     def export_soup_file(self):
-        containList = self.__soup__.find_all(listObj)
+        containList = self.__soup__.find_all(['h1', 'h2', 'h3', 'h4'])
+        print(containList)
+
+        htmltext = '''
+        <html>
+        <head><title>Export Scapped Data</title></head>
+        <body>
+            {SCRAP_LINK}
+        </body>
+        </html>
+        
+        '''
+
+        scrap_link = '<ol>'
+        for tag in containList:
+            if tag.parent.get('href'):
+                link = self.__url__ + tag.parent.get('href')
+                title = tag.string
+                scrap_link += "<li><a href='{}' target=_blank>{}</a></li>\n"
+        scrap_link += '</ol>'
+        htmltext = htmltext.format(SCRAP_LINK=scrap_link)
+
+        self.export_url(filepath='export/final.html', data=htmltext.encode())
     # ------------------------------ #
